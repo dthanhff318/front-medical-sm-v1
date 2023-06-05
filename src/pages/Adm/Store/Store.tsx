0a040203b54766ebd12e1b15de316f7fd996205f
tran <tranvantien02082000@gmail.com>
2023-06-05 23:23:52 +0700

    chi tiet vat tu

--- src/pages/Adm/Store/Store.tsx
+++ src/pages/Adm/Store/Store.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Col, Input, Space, Row, Table, Select, Divider } from 'antd';
 import styles from './style.module.scss';
 import Search from 'antd/es/input/Search';
@@ -7,10 +7,14 @@ import CommonButton from 'components/CommonButton/CommonButton';
 import PaginationCustom from 'components/PaginationCustom/PaginationCustom';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { createQueryUrl } from 'helpers/functions';
-
+import ModalDetailStore from './ModalDetailStore';
+type TModal = '' | 'create';
 const Store: React.FC = () => {
   const navigate = useNavigate();
   const location = useLocation();
+  const [openModal, setOpenModal] = useState<TModal>('');
+  const [listSupply, setListSupply] = useState<any>({});
+
   const { stores, loading, urlQueryParams, pagination, getStore, onDeleteSupplyStore } =
     useService();
 
@@ -80,9 +84,20 @@ const Store: React.FC = () => {
       fixed: 'right',
       width: 100,
       render: (_, record: any) => (
-        <CommonButton danger onClick={() => onDeleteSupplyStore(record.id)}>
-          Xóa
-        </CommonButton>
+        <div className={styles.actionBtn}>
+          <CommonButton
+            onClick={() => {
+              setListSupply(record);
+              console.log(record);
+              setOpenModal('create');
+            }}
+          >
+            Chi tiết
+          </CommonButton>
+          <CommonButton danger onClick={() => onDeleteSupplyStore(record.id)}>
+            Xóa
+          </CommonButton>
+        </div>
       ),
     },
   ];
@@ -99,6 +114,11 @@ const Store: React.FC = () => {
           <Search placeholder="Nhập tên vật tư" onSearch={onSearch} style={{ width: '100%' }} />
         </Col>
       </Row>
+      <ModalDetailStore
+        itemSupply={listSupply}
+        open={openModal === 'create'}
+        onCancel={() => setOpenModal('')}
+      />
       <Table
         columns={columns}
         loading={loading}
