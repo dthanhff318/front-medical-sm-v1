c58ab9a82dfe084887f86df42ec5d2b90437d249
dthanhff <dthanhff318@gmail.com>
2023-07-13 17:30:10 +0700

    nothin

--- src/pages/Adm/Appointment/Appointment.tsx
+++ src/pages/Adm/Appointment/Appointment.tsx
@@ -1,39 +1,20 @@
 import React, { useEffect } from 'react';
 import s from './Home.module.scss';
 import useService from './service';
+import { google } from 'googleapis';
+import { gapi } from 'gapi-script';
 
 // const client = ZoomMtgEmbedded.createClient();
 
 export default function Appointment() {
   const { dataService } = useService();
 
-  // useEffect(() => {
-  //   // Khởi tạo client và cấu hình
-  //   const CLIENT_ID = 'YOUR_CLIENT_ID';
-  //   const API_KEY = 'YOUR_API_KEY';
-  //   const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'];
-  //   const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
-
-  //   gapi.load('client:auth2', () => {
-  //     gapi.client.init({
-  //       apiKey: API_KEY,
-  //       clientId: CLIENT_ID,
-  //       discoveryDocs: DISCOVERY_DOCS,
-  //       scope: SCOPES,
-  //     });
-
-  //     gapi.client.load('calendar', 'v3', () => {
-  //       // API đã sẵn sàng để sử dụng
-  //       // Tiếp theo, bạn có thể triển khai các chức năng của bạn ở đây
-  //     });
-  //   });
-  // }, []);
+  useEffect(() => {}, []);
 
   return (
     <div>
       <h2>Cuộc họp</h2>
-      <div id="meetingSDKElement"></div>
-      <button onClick={() => {}}>Join Meeting</button>
+      <iframe src="https://example.com/meetingsdk" allow="camera; microphone"></iframe>
     </div>
   );
 }
